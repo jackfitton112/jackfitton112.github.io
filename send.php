1365f27703a89c23eaaffbb9d4377d502a6dade2
jackfitton112 <93110524+jackfitton112@users.noreply.github.com>
2021-11-05 00:44:17 +0000

    fixed send.php

---
+++ send.php
@@ -0,0 +1,26 @@
+<?php
+
+include "db.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+$response = $_GET['id'];
+
+if ($response == "h"){
+  $data = "1F60A";
+}
+if ($response == "s"){
+  $data = "1F622";
+}
+if ($response == "d"){
+  $data = "1F633";
+}
+if ($response == "c"){
+  $data = "1F610";
+}
+
+$conn->query("INSERT INTO `emoji` (`id`, `emoji`) VALUES (NULL, '{$data}')");
+
+
+
+ ?>
