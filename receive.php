4e58aa75e908574d27fcd3750ef2771664ef9ff0
jackfitton112 <93110524+jackfitton112@users.noreply.github.com>
2021-11-09 12:24:56 +0000

    send

--- receive.php
+++ receive.php
@@ -7,16 +7,27 @@ header("Access-Control-Expose-Headers: *");
 
 $data = array();
 $result = $conn->query("SELECT * FROM `emoji` ORDER BY `emoji`.`id` ASC");
+$test = $_GET['test'];
 
+if ($test != 1){
 while ($row = $result->fetch_assoc()){
 $data2 = $row['emoji'];
+array_push($data, $data2);
+}
+echo json_encode($data);
+} else {
+
+while ($row = $result->fetch_assoc()){
+$data2 = array("id" => $row['name'], "num" => $row['emoji']);
 array_push($data, $data2);
 
 
 }
+$emotions = array("emotions" => $data);
+echo json_encode($emotions);
+}
 
 
-echo json_encode($data);
 
 
  ?>
